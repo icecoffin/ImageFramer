3a0a4fb7a333ab7c58d4fa4d8167c25738b3f189
Daniil Romatskiy <murkwie@gmail.com>
2019-04-12 23:01:59 +0200

    Add FrameWidthCalculatingTests

---
+++ ImageFramerTests/FrameWidthCalculatingTests.swift
@@ -0,0 +1,38 @@
+//
+//  FrameWidthCalculatingTests.swift
+//  ImageFramerTests
+//
+//  Created by Dani on 12/04/2019.
+//  Copyright © 2019 icecoffin. All rights reserved.
+//
+
+import XCTest
+import Nimble
+@testable import ImageFramer
+
+class FrameWidthCalculatingTests: XCTestCase {
+    func test_percentageFrameWidthCalculator_frameWidth_returnsCorrectValue() {
+        let calculator = PercentageFrameWidthCalculator()
+
+        let frameWidth1 = calculator.frameWidth(forFrameSize: 10, imageMaxSide: 1024)
+        expect(frameWidth1) == 102
+
+        let frameWidth2 = calculator.frameWidth(forFrameSize: 10, imageMaxSide: 1026)
+        expect(frameWidth2) == 103
+    }
+
+    func test_fixedFrameWidthCalculator_frameWidth_returnsCorrectValue() {
+        let calculator = FixedFrameWidthCalculator()
+
+        let frameWidth1 = calculator.frameWidth(forFrameSize: 10, imageMaxSide: 1024)
+        expect(frameWidth1) == 54
+
+        let frameWidth2 = calculator.frameWidth(forFrameSize: 10, imageMaxSide: 1030)
+        expect(frameWidth2) == 55
+    }
+
+    func test_frameWidthCalculatorFactory_makeFrameWidthCalculator_returnsCorrectInstance() {
+        let calculator = FrameWidthCalculatorFactory.makeFrameWidthCalculator()
+        expect(calculator).to(beAnInstanceOf(FixedFrameWidthCalculator.self))
+    }
+}
